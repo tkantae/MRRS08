c9f40f09236e2db0be9ba410951de11236669dcd
Luck0332 <152171778+Luck0332@users.noreply.github.com>
2024-03-19 17:12:09 +0700

    Homepage V.1

--- resources/views/Homepage.blade.php
+++ resources/views/Homepage.blade.php
@@ -12,16 +12,14 @@
             <img src="logo.png" alt="โลโก้ระบบจองห้องประชุม">
             <section class="staff">
                 <a href="staff.html" class="button">เข้าสู่ระบบ</a>
-            </section>
+              </section>
             <h1>Meeting Room <br> Reserve System</h1>
             <h2>ยินดีต้อนรับเข้าสู่ระบบจองห้องประชุม โปรดเลือกการปฏิบัติงาน <br>
                 ที่ท่านต้องการดำเนินการในส่วนถัดไป</h2>
         </header>
         <main>
-            <section class="hero-banner">
+            <section class="buttons">
                 <a href="booking.html" class="button">จองห้องประชุม</a>
-            </section>
-            <section class="status">
                 <a href="status.html" class="button">ตรวจสอบสถานะ</a>
             </section>
         </main>
@@ -33,9 +31,37 @@
             color:#ffffff;
         }
 
+        h1{
+            font-size: 80px;
+            filter: drop-shadow(3px 3px 3px black);
+        }
+
+        h2{
+            font-family: 'Inter', sans-serif; /* เปลี่ยน font-family เป็น 'Inter' */
+        }
+
         body{
             background: linear-gradient(to right, #5E96EB, #FD84F1)
         }
+
+        .staff {
+            float: right;
+            margin-top: 10px;
+        }
+
+        .buttons {
+            display: flex;
+            margin-top: 20px;
+        }
+
+        .buttons .button {
+            display: inline-block;
+            margin: 0 10px;
+        }
+
+
+
+
     </style>
 
 
