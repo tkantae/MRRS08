371735eb82cd54c8339ae039020ccf72be8da1c2
Luck0332 <152171778+Luck0332@users.noreply.github.com>
2024-03-20 16:17:29 +0700

    login V.1

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -22,12 +22,12 @@
 
         <div class="card">
             <div class="card-body login-card-body">
-                <p class="login-box-msg">Sign in to start your session</p>
+                <p class="login-box-msg">Welcome</p>
 
                 <form action="{{ url('login') }}" method="post">
                     @csrf
                     <div class="input-group mb-3">
-                        <input type="email" name="email" class="form-control" placeholder="Email">
+                        <input type="email" name="email" class="form-control" placeholder="Username">
                         <div class="input-group-append">
                             <div class="input-group-text">
                                 <span class="fas fa-envelope"></span>
