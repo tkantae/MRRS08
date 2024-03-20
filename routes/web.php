c03aae5890f2b21922407cd6f40c2851986a1fd0
Luck0332 <152171778+Luck0332@users.noreply.github.com>
2024-03-20 12:29:47 +0700

    เพิ่มหน้ากวนๆ

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/Test', function () {
     return view('Homepage');
 });
 
