59cf70dc46390c928b75871cf20694522a4a1bb4
Luck0332 <152171778+Luck0332@users.noreply.github.com>
2024-04-02 04:27:18 +0700

    sidebar

--- resources/views/layout/Employee.blade.php
+++ resources/views/layout/Employee.blade.php
@@ -11,36 +11,52 @@
 
         @section('sidebar')
         <aside>
-            <p> Menu </p>
-            <a href="Employee">
+            <p href="Homepage"> Menu </p>
+            <a href="Employee" class="sidebar-link ">
               <i class="fa fa-user-o" aria-hidden="true"></i>
               หน้าหลัก
             </a>
-            <a href="Reserve">
+            <a href="Reserve" class="sidebar-link ">
               <i class="fa fa-laptop" aria-hidden="true"></i>
               จองห้อง
             </a>
-            <a href="Petition">
+            <a href="Petition" class="sidebar-link ">
               <i class="fa fa-clone" aria-hidden="true"></i>
               คำร้องขอ
             </a>
-            <a href="Reservation_list">
+            <a href="Reservation_list" class="sidebar-link ">
               <i class="fa fa-star-o" aria-hidden="true"></i>
               รายการจองห้อง
             </a>
-            <a href="Statistics">
+            <a href="Statistics" class="sidebar-link ">
               <i class="fa fa-trash-o" aria-hidden="true"></i>
               สถิติการจอง
             </a>
-            <a href="Manage_account">
+            <a href="Manage_account" class="sidebar-link ">
               <i class="fa fa-trash-o" aria-hidden="true"></i>
               จัดการบัญชี
             </a>
-            <a href="Manage_rooms">
+            <a href="Manage_rooms" class="sidebar-link ">
               <i class="fa fa-trash-o" aria-hidden="true"></i>
               จัดการห้องประชุม
             </a>
           </aside>
+
+          <script>
+            const activeLinks = document.querySelectorAll('.sidebar-link ,[href*="' + window.location.pathname + '"]');
+
+            // วนลูปผ่าน element ทั้งหมด
+            for (const activeLink of activeLinks) {
+                if (activeLink.href.includes(window.location.pathname)) {
+                    // ลบ element ออกจาก sidebar
+                    activeLink.classList.add('active');
+
+                }else{
+                    activeLink.classList.remove('active');
+                }
+            }
+
+          </script>
 
         @show
 
--- resources/views/layout/User.blade.php
+++ resources/views/layout/User.blade.php
@@ -2,7 +2,7 @@
 
 <html>
     <head>
-        <title>App Name - @yield('title')</title>
+        <title>@yield('title')</title>
     </head>
     <body>
 
@@ -10,18 +10,32 @@
 
         @section('sidebar')
         <aside>
-            <p> Menu </p>
-            <a href="titles">
-              <i class="fa fa-user-o" aria-hidden="true"></i>
-              จองห้อง
+            <p href="Homepage"> Menu </p>
+            <a href="User" class="sidebar-link">
+                <i class="fa fa-user-o" aria-hidden="true"></i>
+                จองห้อง
             </a>
-            <a href="follow">
-              <i class="fa fa-laptop" aria-hidden="true"></i>
-              ติดตามสถานะการจอง
+            <a href="follow" class="sidebar-link">
+                <i class="fa fa-laptop" aria-hidden="true"></i>
+                ติดตามสถานะการจอง
             </a>
-
-
         </aside>
+
+        <script>
+            const activeLinks = document.querySelectorAll('.sidebar-link ,[href*="' + window.location.pathname + '"]');
+
+            // วนลูปผ่าน element ทั้งหมด
+            for (const activeLink of activeLinks) {
+                if (activeLink.href.includes(window.location.pathname)) {
+                    // ลบ element ออกจาก sidebar
+                    activeLink.classList.add('active');
+
+                }else{
+                    activeLink.classList.remove('active');
+                }
+            }
+
+          </script>
 
             @show
             <div class="container">
