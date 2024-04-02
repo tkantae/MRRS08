15d82164d44fee585ad8620e003c1ad0eb3b7a45
JengJeng2031 <jakelong2031@hotmail.com>
2024-04-02 18:11:27 +0700

    656

---
+++ resources/views/layout/Status.blade.php
@@ -0,0 +1,26 @@
+@extends('layout.Employee')
+
+    @section('title', 'accout')
+        @section('content')
+            <!DOCTYPE html>
+            <html>
+            <head>
+            <script src="https://kit.fontawesome.com/e71f46c45f.js" crossorigin="anonymous"></script>
+            <link rel="stylesheet" href="{{ url('assets/dist/css/reservation.css') }}">
+            </head>
+            <body>
+            <div class="content-header">
+                @section('status')
+                <i class="fas fa-calendar-days circle-icon-active"></i> 
+                <i class="fas fa-file circle-icon-active"></i>
+                <i class="fas fa-pencil circle-icon-inactive"></i>
+                <i class="fas fa-check circle-icon-inactive"></i>
+                @show
+            </div>
+            <div class="container">
+                    @yield('status')
+                </div>
+            </body>
+            </html>
+    @endsection
+@endsection
