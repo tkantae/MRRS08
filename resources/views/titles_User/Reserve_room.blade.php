4d9c403d03581e88b339767ec21b71d08963603c
JengJeng2031 <jakelong2031@hotmail.com>
2024-04-02 18:07:39 +0700

    eee

--- resources/views/titles_User/Reserve_room.blade.php
+++ resources/views/titles_User/Reserve_room.blade.php
@@ -6,14 +6,19 @@
 <!-- Content Header (Page header) -->
 <div class="content-header">
     <script src="https://kit.fontawesome.com/e71f46c45f.js" crossorigin="anonymous"></script>
-    <i class="fa-solid fa-calendar-days" ></i>
+    <link rel="stylesheet" href="{{ url('assets/dist/css/reservation.css') }}">
+    @section('status')
+    <i class="fas fa-calendar-days circle-icon-active"></i> 
+    <i class="fas fa-file circle-icon-inactive"></i>
+    <i class="fas fa-pencil circle-icon-inactive"></i>
+    <i class="fas fa-check circle-icon-inactive"></i>
+    @show
 </div>
 <!-- /.content-header -->
 
 <!-- Main content -->
 <div class="content">
-    <link rel="stylesheet" href="{{ url('assets/dist/css/reservation.css') }}">
-
+    <!-- Your main content goes here -->
 </div>
 <!-- /.content -->
 
