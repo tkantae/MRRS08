38f02286bd69c61c37abd54ae9aed2d2e3211465
JengJeng2031 <jakelong2031@hotmail.com>
2024-04-02 18:23:48 +0700

    rere

--- resources/views/titles_User/Reserve_room.blade.php
+++ resources/views/titles_User/Reserve_room.blade.php
@@ -5,8 +5,7 @@
 @section('reserv')
 <!-- Content Header (Page header) -->
 <div class="content-header">
-    <script src="https://kit.fontawesome.com/e71f46c45f.js" crossorigin="anonymous"></script>
-    <i class="fa-solid fa-calendar-days" ></i>
+    
 </div>
 <!-- /.content-header -->
 
