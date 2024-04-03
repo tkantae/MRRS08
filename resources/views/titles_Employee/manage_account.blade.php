e061af0b4165951ea80e86f8a36ff2691d8567eb
Boya0828 <kidohagai@gmail.com>
2024-04-03 19:21:44 +0700

    manage_account

--- resources/views/titles_Employee/manage_account.blade.php
+++ resources/views/titles_Employee/manage_account.blade.php
@@ -92,25 +92,21 @@
 
                                 </tbody>
                             </table>
+
                         </div>
 
 
                         <div>
                             <a href="{{ url('/add-user') }}" class="menu-open-button"> + </a>
-
                         </div>
 
                         <br>
                         <br>
                         <br>
                         <!-- /.card-body -->
-                        <div class="card-footer clearfix">
-                            <ul class="pagination pagination-sm m-0 float-right">
-                                <li class="page-item"><a class="page-link" href="#">&laquo;</a></li>
-                                <li class="page-item"><a class="page-link" href="#">1</a></li>
-                                <li class="page-item"><a class="page-link" href="#">2</a></li>
-                                <li class="page-item"><a class="page-link" href="#">3</a></li>
-                                <li class="page-item"><a class="page-link" href="#">&raquo;</a></li>
+                        <div class="card-footer clearfix text-center">
+                            <ul class="pagination pagination-sm m-0"style="display: inline-block;"> 
+                                {!!$users->links('pagination::bootstrap-4')!!}
                             </ul>
                         </div>
                     </div>
