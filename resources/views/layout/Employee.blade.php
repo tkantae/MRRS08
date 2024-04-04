dbe9d5b90735251cc39fe3a113f8f80073ae6d3c
Luck0332 <152171778+Luck0332@users.noreply.github.com>
2024-04-04 19:15:07 +0700

    sidebar_boot

--- resources/views/layout/Employee.blade.php
+++ resources/views/layout/Employee.blade.php
@@ -5,6 +5,12 @@
         <title>@yield('title')</title>
     </head>
     <body>
+                        <!-- Google Font: Source Sans Pro -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+        <!-- Font Awesome -->
+        <link rel="stylesheet" href="{{ url('assets/plugins/fontawesome-free/css/all.min.css') }}">
+        <!-- Theme style -->
+        <link rel="stylesheet" href="{{ url('assets/dist/css/adminlte.min.css') }}">
         <link rel="stylesheet" href="{{ url('assets/plugins/fontawesome-free/css/all.min.css') }}">
         <link rel="stylesheet" href="{{ url('assets/dist/css/sidbar.css') }}">
         @section('sidebar')
