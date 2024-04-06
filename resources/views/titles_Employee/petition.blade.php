dfe21bcdb1b07411af53f2596ef8cd30cc1d343e
shinathorn <152172580+shinathorn@users.noreply.github.com>
2024-04-06 16:28:33 +0700

    newapprove

--- resources/views/titles_Employee/petition.blade.php
+++ resources/views/titles_Employee/petition.blade.php
@@ -2,102 +2,123 @@
 
 @section('title', 'คำร้องขอ')
 @section('content')
-        <link rel="stylesheet" href="{{ url('assets/css.approvelist/approvelist.css') }}">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-        <div class="flex-container">
-            <div>
-                <span class="title">คำขอการจอง</span><br>
-                <span class="number" style="font-size:40px;font-weight: bold; color:rgb(18, 18, 124)">5</span>
-                <span>รายการ</span>
-            </div>
-            <div>
-                <span class="title">คำขอยกเลิก</span><br>
-                <span class="number" style="font-size:40px;font-weight: bold; color:rgb(18, 18, 124)">4</span>
-                <span>รายการ</span>
-            </div>
+    <link rel="stylesheet" href="{{ url('assets/css.approvelist/approvelist.css') }}">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <div class="flex-container">
+        <div>
+            <span class="title">คำขอการจอง</span><br>
+            <span class="number" style="font-size:40px;font-weight: bold; color:rgb(18, 18, 124)">5</span>
+            <span>รายการ</span>
         </div>
-        <br><br>
-        <div class="head">
-            <button id="prev" onclick="changeDataApprove()">คำขอการจอง</button>
-            <button id="next" onclick="changeDataReject()">คำขอยกเลิก</button>
-            <input type="search" placeholder="search" style=";position: relative; left:48%;">
+        <div>
+            <span class="title">คำขอยกเลิก</span><br>
+            <span class="number" style="font-size:40px;font-weight: bold; color:rgb(18, 18, 124)">4</span>
+            <span>รายการ</span>
         </div>
-        <script>
-            function changeDataApprove() {
-            }
+    </div>
+    <br><br>
+    <div class="head">
+        <button id="prev" onclick="changeDataApprove()">คำขอการจอง</button>
+        <button id="next" onclick="changeDataReject()">คำขอยกเลิก</button>
+        <input type="search" placeholder="search" style=";position: relative; left:48%;">
+    </div>
+    <script>
+        function changeDataApprove() {
+            var $test01 = true;
 
-            function changeDataReject() {
-            }
-        </script>
+            // ตรวจสอบว่าค่าถูกเปลี่ยนหรือไม่
+            console.log('ค่าตัวแปร test01 ได้รับการเปลี่ยนแปลงเป็น: ' + $test01);
+        }
 
-        @if ($test01)
-            <table class="rwd-table">
-                <thead>
+        function changeDataReject() {
+            var $test01 = false;
+
+            // ตรวจสอบว่าค่าถูกเปลี่ยนหรือไม่
+            console.log('ค่าตัวแปร test01 ได้รับการเปลี่ยนแปลงเป็น: ' + $test01);
+        }
+    </script>
+    @if ($test01)
+        <table class="rwd-table">
+            <thead>
+                <tr>
+                    <th>ลำดับ</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($reservations as $reservation)
                     <tr>
-                        <th>ลำดับ</th>
-                        <th>วันที่เข้าใช้</th>
-                        <th>ชื่อผู้จอง</th>
-                        <th>ชื่อห้อง</th>
-                        <th>ขนาดห้อง</th>
-                        <th>รอดำเนินการ</th>
-                        <th></th>
+                        <td>{{ $reservation->res_id }}</td>
                     </tr>
-                </thead>
-                <tbody>
-                    @foreach ($reservations as $reservation)
-                        <tr>
-                            <td>{{ $reservation->res_id }}</td>
-                            <td>{{ $reservation->res_startdate }}</td>
-                            <td>{{ $reservation->res_ }}</td>
-                            <td>{{ $reservation->ro_id }}</td>
-                            <td>{{ $reservation->res_typeroom }}</td>
-                            <td> <a href=""><i class="fas fa-check-circle fa-lg" style="color: #63E6BE;"></i></a>
-                                <a href=""><i class="fas fa-times-circle fa-lg" style="color: #ff1a1a;"></i></a>
-                            </td>
-                            <td>
-                                <a><i class="fas fa-info-circle fa-lg" id="detail" style="color: #242424"></i></a>
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-
-        @endif
-
-        <div class="card-footer clearfix text-center">
-            <ul class="pagination pagination-sm m-0">
-                {!! $reservations->links('pagination::bootstrap-4') !!}
-            </ul>
-        </div>
-
-        <div id="popup" class="hidden">
-            <table style="width:100%;">
+                @endforeach
+            </tbody>
+        </table>
+    @else ()
+        <table class="rwd-table">
+            <thead>
                 <tr>
-                    <th colspan="2"
-                        style="background-color: #3b81f2; color: white; text-align: center;border-top-left-radius: 15px;border-top-right-radius: 15px;">
-                        รายละเอียดการจอง</th>
+                    <th>ลำดับ</th>
+                    <th>วันที่เข้าใช้</th>
+                    <th>ชื่อผู้จอง</th>
+                    <th>ชื่อห้อง</th>
+                    <th>ขนาดห้อง</th>
+                    <th>รอดำเนินการ</th>
+                    <th></th>
                 </tr>
-                <tr>
-                    <td style="text-align: center">รายละเอียดการจอง</td>
-                </tr>
-                <tr>
-                </tr>
-            </table>
-            <button type="button" id="close-popup">ปิด Popup</button>
-        </div>
+            </thead>
+            <tbody>
+                @foreach ($reservations as $reservation)
+                    <tr>
+                        <td>{{ $reservation->res_id }}</td>
+                        <td>{{ $reservation->res_startdate }}</td>
+                        <td>{{ $reservation->res_ }}</td>
+                        <td>{{ $reservation->ro_id }}</td>
+                        <td>{{ $reservation->res_typeroom }}</td>
+                        <td> <a href=""><i class="fas fa-check-circle fa-lg" style="color: #63E6BE;"></i></a>
+                            <a href=""><i class="fas fa-times-circle fa-lg" style="color: #ff1a1a;"></i></a>
+                        </td>
+                        <td>
+                            <a><i class="fas fa-info-circle fa-lg" id="detail" style="color: #242424"></i></a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @endif
 
-        <script>
-            const openPopupBtn = document.getElementById("detail");
-            const popup = document.getElementById("popup");
-            const closePopupBtn = document.getElementById("close-popup");
+    <div class="card-footer clearfix text-center">
+        <ul class="pagination pagination-sm m-0">
+            {!! $reservations->links('pagination::bootstrap-4') !!}
+        </ul>
+    </div>
 
-            openPopupBtn.addEventListener("click", () => {
-                popup.classList.add("show");
-            });
+    {{-- <div id="popup" class="hidden">
+        <table style="width:100%;">
+            <tr>
+                <th colspan="2"
+                    style="background-color: #3b81f2; color: white; text-align: center;border-top-left-radius: 15px;border-top-right-radius: 15px;">
+                    รายละเอียดการจอง</th>
+            </tr>
+            <tr>
+                <td style="text-align: center">รายละเอียดการจอง</td>
+            </tr>
+            <tr>
+            </tr>
+        </table>
+        <button type="button" id="close-popup">ปิด Popup</button>
+    </div> --}}
 
-            closePopupBtn.addEventListener("click", () => {
-                popup.classList.remove("show");
-            });
-        </script>
+    {{-- <script>
+        const openPopupBtn = document.getElementById("detail");
+        const popup = document.getElementById("popup");
+        const closePopupBtn = document.getElementById("close-popup");
+
+        openPopupBtn.addEventListener("click", () => {
+            popup.classList.add("show");
+        });
+
+        closePopupBtn.addEventListener("click", () => {
+            popup.classList.remove("show");
+        });
+    </script> --}}
 
 @endsection
