1a5dd8ee027803569e067822efe5d92becdbb2b8
JengJeng2031 <jakelong2031@hotmail.com>
2024-04-06 14:21:20 +0700

    updatecalender-size

--- routes/web.php
+++ routes/web.php
@@ -36,6 +36,7 @@ Route::get('/User', [UserController::class,'getReserve']);
 Route::get('/follow', [UserController::class,'getFollow']);
 Route::get('/searchRoom', [UserController::class,'getSearch']);
 Route::get('/fillInformation', [UserController::class,'getInformation']);
+Route::get('/calender', [UserController::class,'getcalender']);
 
 Route::get('/Employee',[EmployeeController::class,'mainpage']);
 Route::get('/Reserve',[EmployeeController::class,'reserve']);
