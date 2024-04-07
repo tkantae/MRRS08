9bdb7098cd77d4707fe94514cd680e628dbe8421
PekoyamaPeko02 <65160324@go.buu.ac.th>
2024-04-07 10:11:08 +0700

    07042024

--- resources/views/titles_User/room_info.blade.php
+++ resources/views/titles_User/room_info.blade.php
@@ -3,76 +3,66 @@
 @section('title', 'รายละเอียดห้องประชุม')
 
 @section('reserv')
-<!-- Content Header (Page header) -->
 
-
-<!-- /.content-header -->
-
-<!-- Main content -->
-<div class="content">
 <link rel="stylesheet" href="{{ url('assets/dist/css/info.css') }}">
-<a href="" class="original-button">Button</a>
 
+<div class="room-name">
+    <span>ห้องประชุม IF-101</span>
+</div>
 
-
-
-
-  <div class="card-image">
-    <img src="https://i.pinimg.com/736x/b3/b5/b6/b3b5b62ae1e40eb4d21c1859284639cf.jpg" alt="Meeting Room Image"> <!---เหลือลิงค์กับdatabase--->
+<div class="row">
+    <div class="pic" id="pic1">
+        <img src="https://i.pinimg.com/736x/b3/b5/b6/b3b5b62ae1e40eb4d21c1859284639cf.jpg" alt="Meeting Room Image">
     </div>
-    <div class="image2-card">
-        <img src="https://i.pinimg.com/736x/b3/b5/b6/b3b5b62ae1e40eb4d21c1859284639cf.jpg" alt="Meeting Room Image2">
-    </div>
-    <div class="image3-card">
-        <img src="https://i.pinimg.com/736x/b3/b5/b6/b3b5b62ae1e40eb4d21c1859284639cf.jpg" alt="Meeting Room Image3">
-    </div>
+    <span class="pic" id="pic2">
+        <img src="https://i.pinimg.com/736x/b3/b5/b6/b3b5b62ae1e40eb4d21c1859284639cf.jpg" alt="Meeting Room Image">
+    </span>
+    <span class="pic" id="pic3">
+        <img src="https://i.pinimg.com/736x/b3/b5/b6/b3b5b62ae1e40eb4d21c1859284639cf.jpg" alt="Meeting Room Image">
+    </span>
 
-    <div class="location-icon">
-        <i class="fas fa-map-marker-alt"></i>
-        <div class="text">
-            <span>ตึกมังกรฟ้าชั้น 4 </span>
+
+</div>
+
+<div class="texthead" >
+    <span>สิ่งอำนวยความสะดวก</span>
+</div>
+<div class="info-row">
+    <div class="info-box" id="box1">
+        <span class="info">
+            <tr>xxxxxxxxxxxxxxxxxxxxxxxxx</tr></br>
+            <tr>xxxxxxxxxxxxxxxxxxxxxxxxx</tr></br>
+            <tr>xxxxxxxxxxxxxxxxxxxxxxxxx</tr></br>
+            <tr>xxxxxxxxxxxxxxxxxxxxxxxxx</tr></br>
+        </span>
+
+
+
+
+
+    </div>
+    <div class="info-box" id="box2">
+        <div class="row-icon">
+            <span class="icon" id="size-icon">
+                <i class="fa-solid fa-s fa-xl"> ห้องขนาดเล็ก</i>
+            </span>
+
+            <span class="icon">
+                <i class="fa-regular fa-money-bill-1 fa-xl"> ราคา 1270/วัน</i>
+            </span>
+
+            <span class="icon">
+                <i class="fa-solid fa-earth-americas fa-xl"> ห้องประชุมทั่วไป</i>
+            </span>
         </div>
 
-    </div>
-<!---ใช้ .sth justify-content: btw;--->
-    <div class="seat-icon">
-        <i class="fas fa-chair"></i>
-        <div class="text">
-            <span>ตึกมังกรฟ้าชั้น 4 </span>
-        </div>
 
-    </div>
 
-    <div class="area-icon">
-        <i class="fa-solid fa-up-right-and-down-left-from-center"></i>
-        <div class="text">
-            <span>ตึกมังกรฟ้าชั้น 4 </span>
-        </div>
 
-    </div>
-
-    <div class="accessories-icon">
-        <i class="fa-solid fa-laptop"></i>
-        <div class="text">
-            <span>ตึกมังกรฟ้าชั้น 4 </span>
-        </div>
 
     </div>
 
 </div>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ <a href="" class="original-button">จองห้องประชุม</a>
 @endsection
