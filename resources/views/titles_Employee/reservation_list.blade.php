2a9b46acdf08fafb7df8a9ac53e5e3132c1c969b
tanatanJ <152682774+tanatanJ@users.noreply.github.com>
2024-04-07 15:56:37 +0700

    Update reservation_list.blade.php

--- resources/views/titles_Employee/reservation_list.blade.php
+++ resources/views/titles_Employee/reservation_list.blade.php
@@ -2,7 +2,7 @@
 
 @section('title', 'รายการจอง')
     <!-- CSS -->
-    <link rel="stylesheet" href="{{ url('assets/css.approvelist/approved.css') }}">
+    <link rel="stylesheet" href="{{ url('assets/css.approvelist/approvelist.css') }}">
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome -->
@@ -17,60 +17,55 @@
 
 @section('content')
     <div class="flex-container">
+        <div>
+            <span class="title">คำการจอง</span><br>
+            <span class="number" style="font-size:40px;font-weight: bold; color:rgb(18, 18, 124)">5</span>
+            <span>รายการ</span>
+        </div>
+    </div>
+    <div class="head">
         <button id="prev"  style=";position: relative; left:2.5%;">รายการจอง</button>
         <input type="search" placeholder="search" style=";position: relative; left:60%;">
     </div>
 
     <center>
-    <table class="rwd-table" >
+        <table class="rwd-table">
+            <thead>
+                <tr>
+                    <th>ลำดับ</th>
+                    <th>วันที่เข้าใช้</th>
+                    <th>ชื่อผู้จอง</th>
+                    <th>ชื่อห้อง</th>
+                    <th>ขนาดห้อง</th>
+                    <th>รอดำเนินการ</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
 
-        <thead>
-            <tr>
-                <th>ลำดับ</th>
-                <th>รหัสการจอง</th>
-                <th>วันที่เข้าใช้</th>
-                <th>ชื่อห้อง</th>
-                <th>ขนาดห้อง</th>
-                <th></th>
-        </thead>
-        <tbody>
+                <tr>
+                    @foreach ($reservations as $res_id => $reservations)
+                    @if ($reservations->res_status == "A")
+                    <tr>
+                        <td>{{ $res_id + 1 }}</td>
+                        <td>{{ $reservations->res_serialcode }}</td>
+                        <td>{{ $reservations->res_startdate }}</td>
+                        <td>{{ $reservations->reserver_id }}</td>
+                        <td>{{ $reservations->res_typeroom }}</td>
+                        <td>
+                           <button class="btn btn-cancel" onclick="cancelReservation('{{ $reservations->res_id }}')" style="background-color: #dc3545; color: white">ยกเลิก</button>
+                           <a><i class="fas fa-info-circle fa-lg" id="detail" style="color: #242424"></i></a>
+                       </td>
+                    </tr>
 
-            <tr>
-                 @foreach ($reservations as $res_id => $reservations)
-                 @if ($reservations->res_status == "A")
-                 <tr>
-                     <td>{{ $res_id + 1 }}</td>
-                     <td>{{ $reservations->res_serialcode }}</td>
-                     <td>{{ $reservations->res_startdate }}</td>
-                     <td>{{ $reservations->reserver_id }}</td>
-                     <td>{{ $reservations->res_typeroom }}</td>
-                     <td>
-                        <button class="btn btn-cancel" onclick="cancelReservation('{{ $reservations->res_id }}')" style="background-color: #dc3545; color: white">ยกเลิก</button>
-                        <a><i class="fas fa-info-circle fa-lg" id="detail" style="color: #242424"></i></a>
+                    @endif
+
+                @endforeach
+               </tr>
+            </tbody>
+        </table>
 
 
-
-                    </td>
-                 </tr>
-
-                 @endif
-
-             @endforeach
-            </tr>
-        </tbody>
-        <tfoot>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td>
-                    เปลี่ยนหน้าdfsd
-                </td>
-            </tr>
-        </tfoot>
-    </table>
 
     <script>
 
