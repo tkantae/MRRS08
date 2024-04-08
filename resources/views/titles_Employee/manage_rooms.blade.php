8adfd910c60447b81a239a506ca6af66d8dde0d5
Boya0828 <kidohagai@gmail.com>
2024-04-08 15:24:16 +0700

    show detail room

--- resources/views/titles_Employee/manage_rooms.blade.php
+++ resources/views/titles_Employee/manage_rooms.blade.php
@@ -16,9 +16,9 @@
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Document</title>
         <link rel="stylesheet" href="https://cdn.datatables.net/2.0.3/css/dataTables.dataTables.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+            integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-            integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <link rel="stylesheet" href="{{ url('assets/css.buttonadd/add.css') }}">
         <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
         <script src="https://cdn.datatables.net/2.0.3/js/dataTables.js"></script>
@@ -80,55 +80,81 @@
                                 <button type="button" class="btn btn-danger" onclick="deleteUser('{{ $rooms->ro_id }}')"><i
                                         class="fas fa-trash-alt"></i> Delete</button>
                             </form>
-                                <!-- ปุ่ม i และโมดัล -->
-                                <button class="btn btn-default btn-xs" onclick="openModal('{{ $rooms->id }}')">i</button>
-                                <div id="myModal-{{ $rooms->id }}" class="modal">
-                                    <div class="modal-content">
-                                        <span class="close" onclick="closeModal('{{ $rooms->id }}')">&times;</span>
-                                        <div>
-                                            <div class="containerro_name">
-                                                {{ $rooms->ro_name }}
+                            <!-- ปุ่ม i และโมดัล -->
+                            <button class="btn btn-default btn-xs" onclick="openModal('{{ $rooms->id }}')">i</button>
+                            <div id="myModal-{{ $rooms->id }}" class="modal">
+                                <div class="modal-content">
+                                    <span class="close" onclick="closeModal('{{ $rooms->id }}')">&times;</span>
+                                    <div>
+                                        <div class="containerro_name">
+                                            {{ $rooms->ro_name }}
+                                        </div>
+                                        <div class="row">
+                                            {{ $rooms->ro_pic1 }}
+                                        </div>
+                                        <div class="row">
+                                            <div class="col">
+                                                <label>ro_description</label><br>
+                                                <div>
+                                                    {{ $rooms->ro_description }}
+                                                </div>
                                             </div>
-                                            <div>
-                                                {{ $rooms->ro_pic1 }}
+                                            <div class="col">
+                                                <label>ro_size</label><br>
+                                                @php
+                                                    $sizeClass = '';
+
+                                                    if ($rooms->ro_size == 'S') {
+                                                        $sizeClass = 'Small';
+                                                    } elseif ($rooms->ro_size == 'M') {
+                                                        $sizeClass = 'Medium';
+                                                    } else {
+                                                        $sizeClass = 'Large';
+                                                    }
+
+                                                @endphp
+                                                <span class="{{ $sizeClass }}">{{ $sizeClass }}</span><br>
+                                                <br>
+                                                <label>ro_avaliable</label><br>
+                                                @php
+                                                    $statusClass = '';
+                                                    if ($rooms->ro_avaliable == 1) {
+                                                        $statusClass = 'available';
+                                                    } else {
+                                                        $statusClass = 'unavailable';
+                                                    }
+                                                @endphp
+
+                                                <span
+                                                    class="{{ $statusClass }}">{{ $rooms->ro_avaliable == 1 ? 'Available' : 'Unavailable' }}</span>
+                                                <br>
                                             </div>
-                                            <label>ro_size</label><br>
-                                            @if ($rooms->ro_size == 'S')
-                                                Small
-                                            @elseif($rooms->ro_size == 'M')
-                                                Medium
-                                            @else
-                                                Large
-                                            @endif
-                                            <br>
-                                            <label>ro_avaliable</label><br>
-                                            @if ($rooms->ro_avaliable == 1)
-                                                Available
-                                            @else
-                                                Unavailable
-                                            @endif
                                         </div>
                                     </div>
+
+
                                 </div>
+                            </div>
+                            </div>
 
-                                <!-- สคริปต์ JavaScript -->
-                                <script>
-                                    function openModal(roomId) {
-                                        var modal = document.getElementById("myModal-" + roomId);
-                                        modal.style.display = "block";
-                                    }
+                            <!-- สคริปต์ JavaScript -->
+                            <script>
+                                function openModal(roomId) {
+                                    var modal = document.getElementById("myModal-" + roomId);
+                                    modal.style.display = "block";
+                                }
 
-                                    function closeModal(roomId) {
-                                        var modal = document.getElementById("myModal-" + roomId);
-                                        modal.style.display = "none";
-                                    }
+                                function closeModal(roomId) {
+                                    var modal = document.getElementById("myModal-" + roomId);
+                                    modal.style.display = "none";
+                                }
 
-                                    window.onclick = function(event) {
-                                        if (event.target.className == "modal") {
-                                            event.target.style.display = "none";
-                                        }
+                                window.onclick = function(event) {
+                                    if (event.target.className == "modal") {
+                                        event.target.style.display = "none";
                                     }
-                                </script>
+                                }
+                            </script>
 
 
 
