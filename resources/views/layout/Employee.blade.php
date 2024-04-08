0aac927c98be91fe09fe1039fdb2f321a4fd206b
JengJeng2031 <jakelong2031@hotmail.com>
2024-04-08 11:15:19 +0700

    updateManageUser

--- resources/views/layout/Employee.blade.php
+++ resources/views/layout/Employee.blade.php
@@ -13,7 +13,7 @@
         <link rel="stylesheet" href="{{ url('assets/dist/css/adminlte.min.css') }}">
         <link rel="stylesheet" href="{{ url('assets/plugins/fontawesome-free/css/all.min.css') }}">
         <link rel="stylesheet" href="{{ url('assets/dist/css/sidbar.css') }}">
-
+        <script src="https://kit.fontawesome.com/e71f46c45f.js" crossorigin="anonymous"></script>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
         @section('sidebar')
@@ -74,7 +74,7 @@
             }
 
           </script>
-
+          
         <div class="container ">
             @yield('content')
         </div>
